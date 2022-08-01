e55bdedbbb6bace9545bd0d26ad5dc325f403dcc
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-08-01 12:18:41 +0300

    Add_odev2

---
+++ Front-end/Odev/Odev2/script.js
@@ -0,0 +1,43 @@
+let a = 10;
+console.log("a = 8: " + (a == 8) )
+console.log("a = 8: " + (a == 10) )
+
+// ******************************************
+
+
+let b = 8
+console.log("b'nin değeri ve türü 8 mi? " + (b === 8))
+console.log("b'nin değeri ve türü 8 mi? " + (b === "8"))
+
+// ******************************************
+
+let c = 5;
+console.log("c 5'e eşit değil mi? " + (c != 5))
+console.log("c 5'e eşit değil mi? " + (c != 10))
+
+// ******************************************
+
+let d = 20;
+console.log("d'nin değeri ve türü farklı mı?" + (d !== 20))
+console.log("d'nin değeri ve türü farklı mı?" + (d !== "20"))
+
+// ******************************************
+
+let e = 15
+console.log("e daha büyük mü? " + (e > 16))
+console.log("e daha büyük mü? " + (e > 14))
+
+// ******************************************
+
+let f = 15
+console.log("f daha küçük mü? " + (f < 16))
+console.log("f daha küçük mü? " + (f < 14))
+
+// ******************************************
+let yas = 22;
+let yasSinir = (yas >= 18 ) ? "Reşit" : "Reşit Değil"
+console.log(yasSinir)
+
+let yas2 = 17;
+let yasSinir2 = (yas2 >= 18 ) ? "Reşit" : "Reşit Değil"
+console.log(yasSinir2)
